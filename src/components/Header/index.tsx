7f007d79d49ab19aac6f6bbee4128aa1218b8102
Tiago Machado <trsminfo@gmail.com>
2025-06-17 17:11:56 -0300

    continuando as aulas

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,19 @@
 'use client'
 import clsx from 'clsx';
-import classNames from 'clsx';
+
 
 import { useEffect, useState } from "react"
 
 export function Header() {
 
-
-
-
-
     return (
-        <div className={clsx('w-fit mx-auto')}>
-            <h1 className={clsx('text-6xl font-bold hover:text-amber-300 transition duration-1000')}>
-                My Blog
+        <header>
+            <h1 className={clsx('text-4xl/normal font-bold hover:text-amber-300 transition duration-1000 py-8',
+                'sm: text-5xl/normal sm:py-10',
+                'md: text-6xl/normal md:py-12'
+            )}>
+                <a href='#'>The Blog</a>
             </h1>
-        </div>
+        </header>
     )
 }
